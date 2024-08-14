0a37bc4cade0a1859c0616e755d91d389fcf9ec3
robot10 <--->
2024-08-14 16:37:07 +0200

    Created the base files structure of the mod

---
+++ craftitems.lua
@@ -0,0 +1,9 @@
+minetest.register_craftitem("startest:space_crystal", {
+    description = "Space Crystal",
+    inventory_image = "space_crystal.png"
+})
+
+minetest.register_craftitem("startest:dense_space_crystal", {
+    description = "Dense Space Crystal",
+    inventory_image = "dense_space_crystal.png"
+})
---
+++ init.lua
@@ -0,0 +1,6 @@
+print("This file will be run at load time!")
+
+local MP = minetest.get_modpath("startest")
+
+dofile(MP.."/craftitems.lua")
+
